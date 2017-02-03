32c82e48f3aec473a8fa3b8d162084f11fba925b
KAYATHRI <1313050@saec.ac.in>
2017-02-03 19:25:46 +0530

    Add files via upload

---
+++ ideone_Z0muNk.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b,c;
+	scanf("%d,%d,%d",&a,&b,&c);
+	if(a>b)
+	if(a>c)
+	printf("a is big");
+	if(b>a)
+	if(b>c)
+	printf("b is big");
+	else
+	printf("c is big");
+	return 0;
+}
